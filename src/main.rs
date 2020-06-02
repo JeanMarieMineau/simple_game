184deed0985e67ea2f8f81ac37d578462243c38a
Histauce <histauce@laposet.net>
2020-06-02 19:22:00 +0200

    Display image.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
-extern crate sdl2; 
-
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::rect::Point;
+use sdl2::rect::{Point, Rect};
 use std::time::Duration;
- 
-pub fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
- 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+
+use sdl2::image::{self, LoadTexture, InitFlag};
+
+pub fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
+
+    let window = video_subsystem.window("my litle game", 800, 600)
         .position_centered()
         .build()
         .unwrap();
 
-    let mut point1 = Point::new(100, 300);
-    let mut point2 = Point::new(100, 300);
-    let mut point3 = Point::new(100, 300);
-    let center_point = Point::new(400, 300);
- 
-    let mut canvas = window.into_canvas().build().unwrap();
- 
+    let mut canvas = window.into_canvas().build()
+        .unwrap();
+    let (width, height) = canvas.output_size()?;
+
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture("assets/raptor.png")?;
+
+    let position = Point::new(0, 0);
+    let sprite = Rect::new(0, 0, 94, 100);
+
     canvas.set_draw_color(Color::RGB(0, 255, 255));
     canvas.clear();
     canvas.present();
@@ -41,42 +45,14 @@ pub fn main() {
             }
         }
         // The rest of the game loop goes here...
-
-        point1 = next_pos(point1, 10);
-        point2 = next_pos(point2, 1);
-        point3 = next_pos(point3, 100);
-        canvas.set_draw_color(Color::RGB(255, 210, 0));
-        canvas.draw_line(point1, center_point).expect("");
-        canvas.set_draw_color(Color::RGB(0, 210, 255));
-        canvas.draw_line(point2, center_point).expect("");
-        canvas.set_draw_color(Color::RGB(255, 0, 210));
-        canvas.draw_line(point3, center_point).expect("");
-
+        let screen_pos = position + Point::new(width as i32 / 2, height as i32 / 2);
+        let screen_rect = Rect::from_center(screen_pos, sprite.width(), sprite.height());
+        canvas.copy(&texture, sprite, screen_rect)?;
 
 
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
-}
 
-fn next_pos(point: Point, d: i32)->Point{
-    if point.x() == 100 && point.y() == 100{
-        point.offset(d, 0) 
-    }else if point.x() == 700 && point.y() == 100{
-        point.offset(0, d)
-    }else if point.x() == 700 && point.y() == 500{
-        point.offset(-d, 0) 
-    }else if point.x() == 100 && point.y() == 500{
-        point.offset(0, -d)   
-    }else if point.x() == 100{
-        point.offset(0, -d)   
-    }else if point.y() == 100{
-        point.offset(d, 0)    
-    }else if point.x() == 700{
-        point.offset(0, d)  
-    } else if point.y() == 500{
-        point.offset(-d, 0) 
-    }else{
-        panic!("point1 should not be here: ({}, {})", point.x(), point.y());
-    }
+    Ok(())
 }
